03b9632980607abea5dfbb7fabc83aa2f13e2869
Jona <llamasjonat@gmail.com>
2023-08-17 03:49:15 -0300

    fix(component): ts-nocheck added on QrButton

--- src/components/Shared/QrButton/index.tsx
+++ src/components/Shared/QrButton/index.tsx
@@ -1,6 +1,7 @@
-import { Box, Button, Flex } from "@chakra-ui/react";
-import { FunctionComponent } from "react";
-import QR from "src/components/Icons/QR";
+// @ts-nocheck
+import { Box, Button, Flex } from '@chakra-ui/react';
+import { FunctionComponent } from 'react';
+import QR from 'src/components/Icons/QR';
 
 interface QrButtonProps {
   brand?: string;
@@ -8,23 +9,23 @@ interface QrButtonProps {
 }
 
 const Component: FunctionComponent<QrButtonProps> = (props) => {
-  const { brand = "primary", onClick } = props;
+  const { brand = 'primary', onClick } = props;
 
   const boxStyle = {
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-    gap: "8px",
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    gap: '8px',
 
     p: {
-      color: "gray35",
+      color: 'gray35',
     },
   };
 
   const buttonStyle = {
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
     height: 75,
     minWidth: 75,
 
@@ -48,14 +49,14 @@ const Component: FunctionComponent<QrButtonProps> = (props) => {
           borderRadius: 100,
           height: 130,
           width: 130,
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
         }}
         backgroundColor={`${brand}15`}
       >
         <Button {...buttonStyle} {...props} onClick={onClick}>
-          <Box alignItems="center">
+          <Box alignItems='center'>
             <QR />
           </Box>
         </Button>
