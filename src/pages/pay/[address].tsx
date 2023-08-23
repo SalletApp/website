b942ed838b8b21481dc2ab93828fa9279cbe6724
gasparnd <gaspardolcemascolo@gmail.com>
2023-08-23 20:12:47 -0300

    remove log

--- src/pages/pay/[address].tsx
+++ src/pages/pay/[address].tsx
@@ -7,7 +7,6 @@ import Send from "src/components/FullModal/Send";
 const Pay = () => {
   const router = useRouter();
   const address = router.query.address;
-  console.log("🚀 ~ file: [address].tsx:11 ~ Pay ~ address:", address);
   return <Send onClose={() => null} address={address} />;
 };
 
